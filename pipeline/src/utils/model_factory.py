38daef9c4411a4511dcadeb1737dd7836b553e77
mw <woj.maxym@gmail.com>
2025-03-13 00:31:54 -0600

    GNN for glycans and model_configs for getting best  validation set loss yet

--- pipeline/src/utils/model_factory.py
+++ pipeline/src/utils/model_factory.py
@@ -1,12 +1,17 @@
 from typing import Dict, Any
+
 from ..models.glycan_encoders.dummy import DummyGlycanEncoder
 from ..models.glycan_encoders.chemberta import ChemBERTaEncoder
 from ..models.glycan_encoders.rdkit import RDKITGlycanEncoder
+from ..models.glycan_encoders.gnn import GNNGlycanEncoder
+
 from ..models.protein_encoders.dummy import DummyProteinEncoder
 from ..models.binding_predictors.dummy import DummyBindingPredictor
+from ..models.protein_encoders.biopy import BioPyProteinEncoder
+
 from ..models.binding_predictors.dnn import DNNBindingPredictor
 from ..models.protein_encoders.esmc import ESMCEncoder
-from ..models.protein_encoders.biopy import BioPyProteinEncoder
+
 
 
 
@@ -15,7 +20,8 @@ def create_glycan_encoder(encoder_type: str, **kwargs) -> Any:
     encoders = {
         'dummy': DummyGlycanEncoder,
         'chemberta': ChemBERTaEncoder,
-        'rdkit': RDKITGlycanEncoder
+        'rdkit': RDKITGlycanEncoder,
+        'gnn': GNNGlycanEncoder
     }
     
     encoder = encoders[encoder_type]
